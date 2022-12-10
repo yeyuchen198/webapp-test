266e4aef0807ec4a2cc6070f8b83b8efd9f472ed
yuchen1573 <120267390+yuchen1573@users.noreply.github.com>
2022-12-11 02:04:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,4 @@ import subprocess
 
 # print(subprocess.call(("install -m 755 ./xray ./bin/xray"), shell=True))
 
-# print(
-    subprocess.call(("./xray -config  ./config.json"),
-                    shell=True))
+# print(subprocess.call(("./xray -config  ./config.json"), shell=True))
